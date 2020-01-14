bee3557c0a1777daa81fc0c7577c577228a5cd91
Dakota Druley <58957027+dakotadruley@users.noreply.github.com>
2020-01-13 17:38:48 -0800

    commit 2

--- test/example.test.js
+++ test/example.test.js
@@ -1,15 +1,16 @@
 // IMPORT MODULES under test here:
 // import example from '../src/example.js';
+import example from '../is-yes.js';
 
 const test = QUnit.test;
 
-test('time to test a function', function(assert) {
+test('is yes', isYes(assert) {
     //Arrange
     // Set up your parameters and expectations
 
     //Act 
     // Call the function you're testing and set the result to a const
-
+    const 
     //Assert
     // Make assertions about what is expected valid result
     assert.equal(true, false);
