cd2ef5593393e8af2250016d7beefaecb4041cb5
Dakota Druley <58957027+dakotadruley@users.noreply.github.com>
2020-01-15 09:13:34 -0800

    this is a commit on the correctly made new branch - Guess_A_Number

--- test/example.test.js
+++ test/example.test.js
@@ -76,4 +76,49 @@ test('does sting yes return false', (assert) => {
     // Make assertions about what is expected valid result
     assert.equal(result, expected);
 });
+import compareNumbers from '../function.js';
+
+const test = QUnit.test;
+
+test('is the number too high', assert => {
+    //Arrange
+    // Set up your parameters and expectations
+    const guess = 9;
+    const correctNumber = 8; 
+    const expected = 1;
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const result = compareNumbers(guess, correctNumber);
+    //Assert
+    // Make assertions about what is expected valid result
+    assert.equal(expected, result);
+});
+
+test('is the number too low', assert => {
+    //Arrange
+    // Set up your parameters and expectations
+    const guess = 5;
+    const correctNumber = 8; 
+    const expected = -1;
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const result = compareNumbers(guess, correctNumber);
+    //Assert
+    // Make assertions about what is expected valid result
+    assert.equal(expected, result);
+});
+
+test('is the number correct', assert => {
+    //Arrange
+    // Set up your parameters and expectations
+    const guess = 8;
+    const correctNumber = 8; 
+    const expected = 0;
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const result = compareNumbers(guess, correctNumber);
+    //Assert
+    // Make assertions about what is expected valid result
+    assert.equal(expected, result);
+});
 
